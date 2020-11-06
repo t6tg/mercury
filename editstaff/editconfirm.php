d304ce2fe76811d28e8e08c1867af5f2faf94321
Kittidet Cheavachirakul <fvgbhv14@gmail.com>
2020-11-06 19:20:33 +0700

    add edit

--- editstaff/editconfirm.php
+++ editstaff/editconfirm.php
@@ -1,35 +1,25 @@
 <?php
-session_start();
-include "connect.php";
-if ($_SESSION['username'] == null) {
-    echo "<script>alert('กรุณาเข้าสู่ระบบ');</script>";
-    header("Refresh:0 , url=../index.html");
-    exit();
-}
+include "../connect.php";
 
-if ($stmt->execute())
-    header("location: workshop9_9.php");
-
-if ($_POST['name'] != null && $_POST['value'] != null) {
-    $stmt = $pdo->prepare("UPDATE member SET password=?, name=?, address=? , mobile=? , email=? WHERE username=?");
-    $stmt->bindParam(1, $_POST["password"]);
-    $stmt->bindParam(2, $_POST["name"]);
-    $stmt->bindParam(3, $_POST["address"]);
-    $stmt->bindParam(4, $_POST["mobile"]);
-    $stmt->bindParam(5, $_POST["email"]);
-    $stmt->bindParam(6, $_POST["username"]);
-    if ($conn->query($sql)) {
+if ($_POST["staffname"] != null && $_POST["price"] != null) {
+    $stmt = $pdo->prepare("UPDATE staff SET staffname=?, worktime=? , type=? , branch_no=? , price=? WHERE staffno=?");
+    $stmt->bindParam(1, $_POST["staffname"]);
+    $stmt->bindParam(2, $_POST["worktime"]);
+    $stmt->bindParam(3, $_POST["type"]);
+    $stmt->bindParam(4, $_POST["branch_no"]);
+    $stmt->bindParam(5, $_POST["price"]);
+    $stmt->bindParam(6, $_POST["staffno"]);
+    if ($stmt->execute()) {
         echo "<script>alert('แก้ไขสำเร็จ');</script>";
-        header("Refresh:0 , url=editstaff.php");
+        header("Refresh:0 , url=allstaff.php");
         exit();
     } else {
         echo "<script>alert('แก้ไขไม่สำเร็จ');</script>";
-        header("Refresh:0 , url=editstaff.php");
+        header("Refresh:0 , url=allstaff.php");
         exit();
     }
-    echo $_POST['text_todo'];
 } else {
     echo "<script>alert('กรุณากรอกข้อมูล');</script>";
-    header("Refresh:0 , url=editstaff.php");
+    header("Refresh:0 , url=allstaff.php");
     exit();
 }
--- editstaff/editstaff.php
+++ editstaff/editstaff.php
@@ -1,27 +1,38 @@
-<?php include "connect.php" ?>
+<?php include "../connect.php" ?>
 <?php
-$stmt = $pdo->prepare("SELECT * FROM member WHERE username = ?");
-$stmt->bindParam(1, $_GET["username"]);
+$stmt = $pdo->prepare("SELECT * FROM staff WHERE staffno = ?");
+$stmt->bindParam(1, $_GET["staffno"]);
 $stmt->execute();
 $row = $stmt->fetch();
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet" />
 </head>
 
 <body>
-    <form action="workshop9_9editmember.php" method="post">
-        หมายเลขพนักงาน : <input type="text" name="staffno" value="<?= $row["staffno"] ?>"><br><br>
-        ชื่อพนักงาน : <input type="text" name="staffname" value="<?= $row["staffname"] ?>"><br><br>
-        เวลาเข้างาน : <input type="text" name="worktime" value="<?= $row["worktime"] ?>"><br><br>
-        เพศ : <input type="text" name="gender" value="<?= $row["gender"] ?>"><br><br>
-        ประเภท : <input type="text" name="type" value="<?= $row["type"] ?>"><br><br>
-        สาขาที่ทำงาน : <input type="text" name="branch_no" value="<?= $row["branch_no"] ?>"><br><br>
-        ราคา : <input type="text" name="price" value="<?= $row["price"] ?>"><br><br>
-        <input type="submit" value="แก้ไขข้อมูล">
-    </form>
+    <div class="section">
+        <form action="editconfirm.php" method="post">
+            <img src="./img/<?= $row["staffno"] ?>.jpg" alt="Responsive image" width='150' class="rounded"><br><br>
+
+            หมายเลขพนักงาน : <input type="text" name="staff" value="<?= $row["staffno"] ?>" disabled><br><br>
+            <input type="hidden" value="<?= $row["staffno"] ?>" name="staffno" />
+            ชื่อพนักงาน : <input type="text" name="staffname" value="<?= $row["staffname"] ?>"><br><br>
+            เวลาเข้างาน : <input type="time" name="worktime" value="<?= $row["worktime"] ?>"><br><br>
+            ประเภท : <input type="text" name="type" value="<?= $row["type"] ?>"><br><br>
+            สาขาที่ทำงาน : <input type="text" name="branch_no" value="<?= $row["branch_no"] ?>"><br><br>
+            ราคา : <input type="text" name="price" value="<?= $row["price"] ?>"><br><br>
+            <input type="submit" value="แก้ไขข้อมูล">
+        </form>
+    </div>
 </body>
 
 </html>
