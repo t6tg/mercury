c03ee44cbb40379a3cf2d71027528ccdefad4cdf
thanawatgulati <thanawatgulatijames@gmail.com>
2020-11-07 01:16:04 +0700

    😡

--- main/connect.php
+++ main/connect.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:host=localhost:3306;dbname=mercury;charset=utf8", "root", "jamesWebDev");
+$pdo = new PDO("mysql:host=40.78.133.169:3306;dbname=mercury;charset=utf8", "james", "jamesWebDev1*");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
