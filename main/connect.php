778053c45b05985a4cc74bd331ca4c239b5554f1
thanawatgulati <thanawatgulatijames@gmail.com>
2020-11-07 12:22:11 +0700

    Init

--- main/connect.php
+++ main/connect.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=mercury;charset=utf8", "root", "");
+$pdo = new PDO("mysql:host=localhost;dbname=mercury;charset=utf8", "root", "jamesWebDev");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
