f716c720ab4ab67f6d94dfa93900ab4ba990a258
thanawatgulati <thanawatgulatijames@gmail.com>
2020-11-07 13:45:56 +0700

    🙄

--- main/admin/editstaff.php
+++ main/admin/editstaff.php
@@ -21,10 +21,11 @@ $row = $stmt->fetch();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>แก้ไขพนักงาน</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
     <link rel="stylesheet" href="style.css" />
     <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet" />
+    <link rel="icon" href="mer.jpg">
 </head>
 
 <body>
@@ -34,12 +35,13 @@ $row = $stmt->fetch();
 
             หมายเลขพนักงาน : <input type="text" name="staff" value="<?= $row["staffno"] ?>" disabled><br><br>
             <input type="hidden" value="<?= $row["staffno"] ?>" name="staffno" />
-            ชื่อพนักงาน : <input type="text" name="staffname" value="<?= $row["staffname"] ?>"><br><br>
+            ชื่อพนักงาน : <input type="text" name="staffname" value="<?= $row["staffname"] ?>" pattern="[A-Za-z0-9ก-์]{2,50}"><br><br>
             เวลาเข้างาน : <input type="time" name="worktime" value="<?= $row["worktime"] ?>"><br><br>
-            ประเภท : <input type="text" name="type" value="<?= $row["type"] ?>"><br><br>
-            สาขาที่ทำงาน : <input type="text" name="branch_no" value="<?= $row["branch_no"] ?>"><br><br>
-            ราคา : <input type="text" name="price" value="<?= $row["price"] ?>"><br><br>
-            <input type="submit" value="แก้ไขข้อมูล">
+            ประเภท : <input type="text" name="type" value="<?= $row["type"] ?>" pattern="[A-Za-z0-9ก-์]{2,50}"><br><br>
+            สาขาที่ทำงาน : <input type="text" name="branch_no" value="<?= $row["branch_no"] ?>" pattern="[A-Za-z0-9]{5,15}"><br><br>
+            ราคา : <input type="text" name="price" value="<?= $row["price"] ?>" pattern="[0-9]{1,20}"><br><br>
+            <button type="submit">แก้ไขข้อมูล</button>
+
         </form>
     </div>
 </body>
